3e3eef602a6c3b78f624b3774427c48c8b3e632f
LuoYongJ <2020412840@qq.com>
2021-11-20 22:33:50 +0800

    这是第六周的项目文件

--- js/index.js
+++ js/index.js
@@ -35,16 +35,17 @@ $(function() {
 
     /*新书列表手风琴效果 */
     $('.ebooks .right-box ul > li').mouseenter(function() {
-            //所有兄弟：隐藏详情，显示标题
-            $(this).siblings().find('.desc').hide(); //隐藏详情
-            $(this).siblings().find('.ebooks-title').show(); //显示标题
+        //所有兄弟：隐藏详情，显示标题
+        $(this).siblings().find('.desc').hide(); //隐藏详情
+        $(this).siblings().find('.ebooks-title').show(); //显示标题
 
-            //当前标题：隐藏标题，显示详情
-            $(this).find('.ebooks-title').hide(); //隐藏标题
-            $(this).find('.desc').show(); //显示详情
+        //当前标题：隐藏标题，显示详情
+        $(this).find('.ebooks-title').hide(); //隐藏标题
+        $(this).find('.desc').show(); //显示详情
 
-        })
-        /* 服装电子书小轮播 */
+    })
+
+    /* 服装电子书小轮播 */
     $('#content-banner').tyslide({
         boxh: 330, //盒子的高度
         w: 426, //盒子的宽度
@@ -86,5 +87,43 @@ $(function() {
         controlsColor: "#d7d7d7", //普通控制按钮的颜色
         controlsCurrentColor: "#00ff00", //当前控制按钮的颜色
     });
+
+
+
+    /*推广商品切换*/
+    $('.promotion .promotion-title ul li').mouseenter(function() {
+        //导航激活类的切换
+        $(this).addClass('active').siblings().removeClass('active')
+
+        //内容切换
+        //获取对应的索引
+        var index = $(this).index(); //0 => left:0,   1 => left: -1170
+        console.log(index)
+        console.log($('.promotion .promotion-content .inner-box'))
+
+        //左右移动
+        $('.promotion .promotion-content .inner-box').animate({
+            'left': -index * 1170
+        }, 300)
+    })
+
+    /*返回顶部*/
+    //绑定滚动事件
+    $(document).scroll(function() {
+            //获取距离顶部的距离
+            var topDistance = $('html, body').scrollTop();
+            //判断
+            if (topDistance > 500) {
+                $('.backtotop').fadeIn();
+            } else {
+                $('.backtotop').fadeOut();
+            }
+        })
+        //返回顶部功能
+    $('.backtotop').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 300)
+    })
 
 })
